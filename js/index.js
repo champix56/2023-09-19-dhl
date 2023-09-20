f9bcf55894f28dbc3efc84e976f5ac25e0936f4c
champix56 <desorbaix@free.fr>
2023-09-20 10:21:10 +0100

    mesure du temps

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 function initJs(color) {
-    console.log(arguments);
-    console.trace(arguments);
-    console.warn(arguments);
-    console.error(arguments);
+  console.time("fnInitJs");
+  console.log(arguments);
+  console.trace(arguments);
+  console.warn(arguments);
+  console.error(arguments);
 
   var jsLoadedNode = document.querySelector("#is-js-loaded");
   jsLoadedNode.innerHTML = "JS <b>OK</b>";
   jsLoadedNode.style.textAlign = "center";
   jsLoadedNode.style.color = color;
+  console.timeEnd("fnInitJs");
 }
-initJs('aquamarine',{a:34,b:"eee"});
+initJs("aquamarine", { a: 34, b: "eee" });
