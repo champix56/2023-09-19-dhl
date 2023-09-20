6dcb7554c0b8894e70607967d73533b7856882f8
champix56 <desorbaix@free.fr>
2023-09-20 10:07:04 +0100

    declaration de fonction + execution de la function

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
-var jsLoadedNode = document.querySelector("#is-js-loaded");
-jsLoadedNode.innerHTML = "JS <b>OK</b>";
-jsLoadedNode.style.textAlign = "center";
-jsLoadedNode.style.color = "tomato";
+function initJs() {
+  var jsLoadedNode = document.querySelector("#is-js-loaded");
+  jsLoadedNode.innerHTML = "JS <b>OK</b>";
+  jsLoadedNode.style.textAlign = "center";
+  jsLoadedNode.style.color = "tomato";
+}
+initJs();
