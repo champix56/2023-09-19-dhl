8d617383bb2c82ec33cc766e69b8627379280dfb
champix56 <desorbaix@free.fr>
2023-09-20 16:27:02 +0100

    suite

--- js/Meme.js
+++ js/Meme.js
@@ -1,16 +1,29 @@
 /**
- * Constructeur d'objet Meme
+ * construction d'instance meme avec ou sans json de base
+ * @param {string} jsonConfiguredMemeStr
  */
-function Meme(configuredMeme) {
-    this.texte = "";
-    this.x = 0;
-    this.y = 32;
-    this.color = "#000000";
-    this.fontWeight = "500";
-    this.fontSize = 32;
-    this.underline = false;
-    this.italic = false;
-    this.imageId=-1 
+function Meme(jsonConfiguredMemeStr) {
+  this.texte = "";
+  this.x = 0;
+  this.y = 32;
+  this.color = "#000000";
+  this.fontWeight = "500";
+  this.fontSize = 32;
+  this.underline = false;
+  this.italic = false;
+  this.imageId = -1;
+  /**
+   * chargement de valeurs a partir d'un meme en json
+   * @param {string} jsonStr
+   */
+  this.loadFromString = function (jsonStr) {
+    Object.assign(this, JSON.parse(jsonStr));
+  };
+
+
+  if(jsonConfiguredMemeStr!==undefined){
+    this.loadFromString(jsonConfiguredMemeStr);
+  }
 }
 
-var meme=new Meme();
+var meme = new Meme();
