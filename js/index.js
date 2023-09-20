8e54c70ec32ab22df9a333ce5b186bc2ee16daac
champix56 <desorbaix@free.fr>
2023-09-20 10:21:35 +0100

    delete des console

--- js/index.js
+++ js/index.js
@@ -1,14 +1,7 @@
 function initJs(color) {
-  console.time("fnInitJs");
-  console.log(arguments);
-  console.trace(arguments);
-  console.warn(arguments);
-  console.error(arguments);
-
   var jsLoadedNode = document.querySelector("#is-js-loaded");
   jsLoadedNode.innerHTML = "JS <b>OK</b>";
   jsLoadedNode.style.textAlign = "center";
   jsLoadedNode.style.color = color;
-  console.timeEnd("fnInitJs");
 }
 initJs("aquamarine", { a: 34, b: "eee" });
