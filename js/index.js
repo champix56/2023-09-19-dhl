330f2a7a148fc3287c33dcbf7453a2ee1e633c9a
champix56 <desorbaix@free.fr>
2023-09-20 10:17:43 +0100

    console debug log warn error

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
-function initJs() {
+function initJs(color) {
+    console.log(arguments);
+    console.trace(arguments);
+    console.warn(arguments);
+    console.error(arguments);
+
   var jsLoadedNode = document.querySelector("#is-js-loaded");
   jsLoadedNode.innerHTML = "JS <b>OK</b>";
   jsLoadedNode.style.textAlign = "center";
-  jsLoadedNode.style.color = "tomato";
+  jsLoadedNode.style.color = color;
 }
-initJs();
+initJs('aquamarine',{a:34,b:"eee"});
